6d36d504cce91b88292955f309de7543944dc8aa
Arun172452 <arunkumar.lekkalapudi@gmail.com>
2021-07-16 12:20:12 +0530

    Integrated DynamoDB

--- vm-lambda/getInstance/fetchId.go
+++ vm-lambda/getInstance/fetchId.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"encoding/json"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -38,6 +39,16 @@ type instanceInfo struct {
 	Name             string  `json:"name"`
 }
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var vmList []SystemGCVM
+	var instanceList []string
+	err1 := table.Scan().All(&vmList)
+	if err1 != nil {
+		fmt.Println("Error in fetching data from dynamoDB", err1.Error())
+		return events.APIGatewayProxyResponse{Body: "Error in fetching data from dynamoDB", StatusCode: 500}, err1
+	}
+	for _,vm := range vmList {
+		instanceList = append(instanceList,vm.InstanceId)
+	}
 	//Type of vm - aws/onpremise
 	environment := request.PathParameters["name"]
 	var instanceSummary []instanceInfo
@@ -59,18 +70,26 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 			return events.APIGatewayProxyResponse{Body: "there was an error listing instances", StatusCode: 500}, err
 		}
 		for idx := range resp.Reservations {
+			nonStandard := "no"
+			tagName := "NoName"
 			for _, inst := range resp.Reservations[idx].Instances {
 				if(*inst.State.Code == 48){
 					continue
 				}
+				if(contains(instanceList,*inst.InstanceId)==true){
+					continue
+				}
 				for _, tag := range inst.Tags {
-					if (*tag.Key == "Name") || (*tag.Key == "name") {
-						instanceDe := instanceInfo{InstanceId:*inst.InstanceId,Name:*tag.Value}
-						instanceSummary = append(instanceSummary,instanceDe)
-					} else {
-						instanceDe := instanceInfo{InstanceId:*inst.InstanceId,Name:"NoName"}
-						instanceSummary = append(instanceSummary,instanceDe)
+					if (*tag.Key == "Schedule"){
+						nonStandard = "yes"
 					}
+					if (*tag.Key == "Name") || (*tag.Key == "name") {
+						tagName = *tag.Value
+					}
+				}
+				if(nonStandard == "no"){
+					instanceDe := instanceInfo{InstanceId:*inst.InstanceId,Name:tagName}
+					instanceSummary = append(instanceSummary,instanceDe)
 				}
 			
 			}
@@ -97,6 +116,15 @@ func connectDb() {
 	sess := session.Must(session.NewSession())
 	db := dynamo.New(sess, &aws.Config{Region: aws.String(region)})
 	table = db.Table(tableName)
+}
+
+func contains(instanceList []string, instanceId string) bool {
+    for _, a := range instanceList {
+        if a == instanceId {
+            return true
+        }
+    }
+    return false
 }
 
 func main() {
--- vm-lambda/go.mod
+++ vm-lambda/go.mod
@@ -5,5 +5,7 @@ go 1.16
 require (
 	github.com/aws/aws-lambda-go v1.8.1
 	github.com/aws/aws-sdk-go v1.40.0
+	github.com/google/uuid v1.3.0
+	github.com/guregu/dynamo v1.10.4
 	github.com/stretchr/testify v1.7.0 // indirect
 )
