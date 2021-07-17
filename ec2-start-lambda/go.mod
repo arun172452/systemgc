9831e7f99db021d8bde9112d28e98165de02fe26
Arun172452 <arunkumar.lekkalapudi@gmail.com>
2021-07-17 11:34:25 +0530

    added vm start listerners

---
+++ ec2-start-lambda/go.mod
@@ -0,0 +1,11 @@
+module github.com/examples/aws-golang-s3-file-replicator
+
+go 1.14
+
+require (
+	github.com/aws/aws-lambda-go v1.6.0
+	github.com/aws/aws-sdk-go v1.40.2
+	github.com/google/uuid v1.3.0
+	github.com/sirupsen/logrus v1.8.1
+	github.com/stretchr/testify v1.5.1 // indirect
+)
---
+++ ec2-start-lambda/src/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"os"
+	"time"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	guuid "github.com/google/uuid"
+	log "github.com/sirupsen/logrus"
+)
+
+var (
+	region = os.Getenv("aws_region")
+)
+
+type SystemGCVM struct {
+	UUID           guuid.UUID `json:"uuid"`
+	UserId         string     `json:"userid"`
+	VmName         string     `json:"vmname"`
+	VmType         string     `json:"vmtype"`
+	InstanceId     string     `json:"instanceid,omitempty" ,dynamo:",omitempty"`
+	IpAddress      string     `json:"ipaddress,omitempty" ,dynamo:",omitempty"`
+	SanCleanUp     string     `json:"sancleanup,omitempty" ,dynamo:",omitempty"`
+	AgentInstalled string     `json:"agent,omitempty" ,dynamo:",omitempty"`
+	SshKeyPath     string     `json:"keypath,omitempty" ,dynamo:",omitempty"`
+	CreationDate   time.Time  `json:"creation_date,omitempty" ,dynamo:",omitempty"`
+}
+
+func main() {
+	lambda.Start(handler)
+}
+
+func handler(ctx context.Context, snsEvent events.SNSEvent) {
+	ec2svc := ec2.New(session.New(&aws.Config{
+		Region: aws.String(region)}))
+	input := &ec2.StartInstancesInput{
+		InstanceIds: []*string{},
+	}
+	for _, record := range snsEvent.Records {
+		var u SystemGCVM
+		err := json.Unmarshal([]byte(record.SNS.Message), &u)
+		if err != nil {
+			log.Errorf("ERROR Unmarshal Body %s: %s", record.SNS.Message, err)
+		}
+
+		input.InstanceIds = append(input.InstanceIds, aws.String(u.InstanceId))
+	}
+
+	result, err := ec2svc.StartInstances(input)
+	if err == nil {
+		log.Println(result)
+
+	} else {
+		log.Println(err.Error())
+	}
+}
